9202faaf3f7fa9cd1be42aacf8feadd6730a9333
gusm217 <gusm217@gmail.com>
2023-10-31 18:53:53 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,11 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamErrors('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamErrors('email'))
+    const fieldsRequired = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of fieldsRequired) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamErrors(field))
+      }
     }
   }
 }
